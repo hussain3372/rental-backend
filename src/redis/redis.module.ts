e32321872de7ca50054f35e8bc35731f5f7fe2ba
hussain3372 <124873500+hussain3372@users.noreply.github.com>
2025-10-08 17:27:21 +0500

    Update redis.module.ts

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -1,9 +1,7 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
 import { RedisService } from './redis.service';
 
 @Module({
-  imports: [ConfigModule],
   providers: [RedisService],
   exports: [RedisService], // export so other modules can use it
 })
